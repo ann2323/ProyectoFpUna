5c306482b67cf21d8d05a1f92dece7257d375e0d
Any <Any@AnaPaula>
2015-12-08 16:56:43 -0300

    CreditoForm

--- src/vista/NotaCreditoCompraForm.java
+++ src/vista/NotaCreditoCompraForm.java
@@ -77,11 +77,12 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
     
     Stock stock = new Stock();
     
-     Integer subTotal= 0, totaldesc=0;
+     Integer subTotal= 0, totaldesc=0,nroFacturaAux=0 ;
      double iva10=0.0, iva5=0.0;
      Integer  cantProducto=0;
      int k, k2, factRef;
      DecimalFormat formateador = new DecimalFormat("###,###.##");
+     String estado="";
      
     StockControlador stockCont = new StockControlador();
     DepositoControlador depBD = new DepositoControlador();
@@ -332,6 +333,7 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
             }
             compraC.setNroPrefijo(txtPrefijoCompra.getText());
             compraC.setNroFactura(Integer.parseInt(txtFacturaCompra.getText()));
+            nroFacturaAux=Integer.parseInt(txtFacturaCompra.getText());
             Date ahora = new Date();
             compraC.setFechaRecepcion(ahora);
             SimpleDateFormat formateador = new SimpleDateFormat("dd-MM-yyyy");
@@ -341,6 +343,7 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
             compraC.setPagoContado("");
             compraC.setFactReferenciada(Integer.parseInt(txtFacturaReferenciada.getText().trim()));
             compraC.setEstado("BORRADOR");
+            estado=compraC.getEstado();
             int idProveedor = provC.devuelveId(txtProveedor.getText().replace(".", ""));
             compraC.setProveedorId(idProveedor);
             Deposito dep = (Deposito) this.JCdeposito.getSelectedItem();
@@ -442,7 +445,7 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
                     }
                     
                      if (bNuevo.isEnabled() == false){ 
-                        compraC.setEstado("CONFIRMADO");
+                        compraC.setEstado("BORRADOR");
                         compraControlador.insert(compraC);
                         txtFacturaCompra.setText("");
                         nuevo();
@@ -928,9 +931,6 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
         setTitle("Nota Credito Compra");
         setFrameIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/1434930447_data-edit.png"))); // NOI18N
         addInternalFrameListener(new javax.swing.event.InternalFrameListener() {
-            public void internalFrameOpened(javax.swing.event.InternalFrameEvent evt) {
-                formInternalFrameOpened(evt);
-            }
             public void internalFrameActivated(javax.swing.event.InternalFrameEvent evt) {
             }
             public void internalFrameClosed(javax.swing.event.InternalFrameEvent evt) {
@@ -942,6 +942,9 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
             public void internalFrameDeiconified(javax.swing.event.InternalFrameEvent evt) {
             }
             public void internalFrameIconified(javax.swing.event.InternalFrameEvent evt) {
+            }
+            public void internalFrameOpened(javax.swing.event.InternalFrameEvent evt) {
+                formInternalFrameOpened(evt);
             }
         });
 
@@ -1528,7 +1531,7 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
             if ("*".equals(txtProveedor.getText())) {
 
                 BuscarForm bf = new BuscarForm( null, true);
-                bf.columnas = "substring(ci from 1 for 1)||'.'||substring(ci from 2 for 3)||'.'||substring(ci from 5 for 7) as \"CI\", nombre||' '||apellido as \"Proveedor\"";
+                bf.columnas = "trim(to_char(cast(ci as integer),'9G999G999')) as \"CI\", nombre||' '||apellido as \"Proveedor\"";
                 bf.tabla = "proveedor";
                 bf.order = "proveedor_id";
                 bf.filtroBusqueda = "estado!='INACTIVO'";
@@ -1551,6 +1554,9 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
     private void jBGuardar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBGuardar1ActionPerformed
      try {
          guardar();
+         if (estado.equals("BORRADOR")){
+         compraControlador.updateEstado(nroFacturaAux);
+         }
      } catch (Exception ex) {
          Logger.getLogger(NotaCreditoCompraForm.class.getName()).log(Level.SEVERE, null, ex);
      }
@@ -1641,24 +1647,26 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_txtFacturaCompraKeyTyped
 
     private void txtFacturaCompraKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtFacturaCompraKeyPressed
+                 
         if (txtProveedor.isEnabled() == true){
               return ;
         }  
          else{
         if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+
             if ("*".equals(txtFacturaCompra.getText())) {
                 //TBdetalleCuenta2.setRowSelectionInterval(0,0);
                 BuscarForm bf = new BuscarForm( null, true);
-                bf.columnas = "v.nro_factura";
-                bf.tabla = "compra v";
-                bf.order = "v.nro_factura";
+                bf.columnas = "nro_prefijo as \"Nro. Prefijo\", trim(to_char(cast(nro_factura as integer),'9G999G999')) as \"Nro. Factura\"";
+                bf.tabla = "Compra";
+                bf.order = "nro_factura";
                 bf.filtroBusqueda = "es_factura = 'N' and estado = 'BORRADOR'"; //factura en suspension. Solo los que esten en estado Borrador
                 bf.setLocationRelativeTo(this);
                 bf.setVisible(true);
                 
 
                 for(int c=0; c<modeloNroFactura.getRowCount(); c ++){
-                    if (modeloNroFactura.getValueAt(c, 1).toString().equals(bf.retorno)){
+                    if (modeloNroFactura.getValueAt(c, 0).toString().equals(bf.retorno)){
                         modoBusqueda(false);
                         establecerBotones("Edicion");
                         k2 = c;
@@ -1672,7 +1680,7 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
             getNroFactura();
             
             for(int c=0; c<modeloNroFactura.getRowCount(); c ++){
-                if (modeloNroFactura.getValueAt(c, 1).toString().equals(txtFacturaCompra.getText())){
+                if (modeloNroFactura.getValueAt(c, 0).toString().equals(txtFacturaCompra.getText())){
                     modoBusqueda(false);
                     establecerBotones("Edicion");
                     k2 = c;                   
@@ -1707,16 +1715,16 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
             if ("*".equals(txtFacturaReferenciada.getText())) {
                 //TBdetalleCuenta2.setRowSelectionInterval(0,0);
                 BuscarForm bf = new BuscarForm( null, true);
-                bf.columnas = "v.nro_factura";
-                bf.tabla = "compra v";
-                bf.order = "v.nro_factura";
+                bf.columnas = "nro_prefijo as \"Nro. Prefijo\", trim(to_char(cast(nro_factura as integer),'9G999G999')) as \"Nro. Factura\"";
+                bf.tabla = "Compra";
+                bf.order = "nro_factura";
                 bf.filtroBusqueda = "es_factura = 'S' and estado = 'PENDIENTE' or estado = 'CONFIRMADO'"; //factura en suspension. Solo los que esten en estado Borrador
                 bf.setLocationRelativeTo(this);
                 bf.setVisible(true);
                 
 
                 for(int c=0; c<modeloNroFactura.getRowCount(); c ++){
-                    if (modeloNroFactura.getValueAt(c, 1).toString().equals(bf.retorno)){
+                    if (modeloNroFactura.getValueAt(c, 0).toString().equals(bf.retorno)){
                         modoBusqueda(false);
                         establecerBotones("Nuevo");
                         k2 = c;
@@ -1730,7 +1738,7 @@ public class NotaCreditoCompraForm extends javax.swing.JInternalFrame {
             getNroFacturaPagadas();
        
             for(int c=0; c<modeloNroFactura.getRowCount(); c ++){
-                if (modeloNroFactura.getValueAt(c, 1).toString().equals(txtFacturaCompra.getText())){
+                if (modeloNroFactura.getValueAt(c, 0).toString().equals(txtFacturaCompra.getText())){
                     modoBusqueda(false);
                     establecerBotones("Edicion");
                     k2 = c;                   
